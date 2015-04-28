bf63cc0521b90650cd0a8e35d773d90d3d639fcf
rickparrish <github@rick.parrish>
2015-04-28 14:47:58 -0400

    Change the way we handle IE < 9 error suppression (again makes it easier for "widget" usage)

---
+++ release/IELessThan9Check.js
@@ -0,0 +1,9 @@
+﻿// IE less than 9.0 will throw script errors and not even load
+if (navigator.appName === 'Microsoft Internet Explorer') {
+    var Version = -1;
+    var RE = new RegExp('MSIE ([0-9]{1,}[\\.0-9]{0,})');
+    if (RE.exec(navigator.userAgent) !== null) { Version = parseFloat(RegExp.$1); }
+    if (Version < 9.0) {
+        Object.defineProperty = function () { /* Do Nothing */ };
+    }
+}
