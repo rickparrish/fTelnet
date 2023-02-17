0556d77bccb91b1f9eaf8e8d582358a870d877e0
Aleksei Faians <af.git@bsrealm.net>
2023-02-17 09:47:56 +0300

    Fix path separator to make things work in Unix environment. This should work in Windows as well because it supports forward slashes.

--- postbuild.js
+++ postbuild.js
@@ -18,7 +18,7 @@ function combineFiles(inputFilenames, outputFilename) {
 				}
 
 				console.log('Combined files into ' + outputFilename);
-				
+
 				resolve();
 			});
 		});
@@ -27,19 +27,19 @@ function combineFiles(inputFilenames, outputFilename) {
 
 function fixEnumerable(filename) {
 	return new Promise((resolve, reject) => {
-		fs.readFile(filename, 'utf8', function (err,data) {
+		fs.readFile(filename, 'utf8', function (err, data) {
 			if (err) {
 				throw err;
 			}
-		  
+
 			data = data.replace(/enumerable[:] false/g, 'enumerable: true');
 			fs.writeFile(filename, data, function (err) {
 				if (err) {
 					throw err;
 				}
-				
+
 				console.log("Fixed 'enumerable: false' in " + filename);
-				
+
 				resolve();
 			});
 		});
@@ -48,21 +48,21 @@ function fixEnumerable(filename) {
 
 function minifyFile(filename) {
 	return new Promise((resolve, reject) => {
-		fs.readFile(filename, 'utf8', function (err,data) {
+		fs.readFile(filename, 'utf8', function (err, data) {
 			if (err) {
 				throw err;
 			}
-		  
+
 			data = jsmin(data);
 			const newFilename = filename.substr(0, filename.lastIndexOf(".")) + ".min.js";
-			
+
 			fs.writeFile(newFilename, data, function (err) {
 				if (err) {
 					throw err;
 				}
-				
+
 				console.log("Minified " + newFilename);
-				
+
 				resolve();
 			});
 		});
@@ -70,26 +70,25 @@ function minifyFile(filename) {
 }
 
 Promise.all([
-	fixEnumerable('stage\\common.js'),
-	fixEnumerable('stage\\connections.js'),
-	fixEnumerable('stage\\crt.js'),
-	fixEnumerable('stage\\crtcontrols.js'),
-	fixEnumerable('stage\\filetransfer.js'),
-	fixEnumerable('stage\\ftelnetclient.js'),
-	fixEnumerable('stage\\graph.js')
-]).then(function() {
+	fixEnumerable('stage/common.js'),
+	fixEnumerable('stage/connections.js'),
+	fixEnumerable('stage/crt.js'),
+	fixEnumerable('stage/crtcontrols.js'),
+	fixEnumerable('stage/filetransfer.js'),
+	fixEnumerable('stage/ftelnetclient.js'),
+	fixEnumerable('stage/graph.js')
+]).then(function () {
 	Promise.all([
-		combineFiles(['stage\\common.js', 'stage\\crt.js', 'stage\\connections.js', 'stage\\ftelnetclient.js'], 'release\\ftelnet.norip.noxfer.js'),
-		combineFiles(['include\\blob.js', 'include\\filesaver.js', 'stage\\common.js', 'stage\\crt.js', 'stage\\connections.js', 'stage\\crtcontrols.js', 'stage\\filetransfer.js', 'stage\\ftelnetclient.js'], 'release\\ftelnet.norip.xfer.js'),
-		combineFiles(['stage\\common.js', 'stage\\crt.js', 'stage\\connections.js', 'stage\\graph.js', 'stage\\ftelnetclient.js'], 'release\\ftelnet.rip.noxfer.js'),
-		combineFiles(['include\\blob.js', 'include\\filesaver.js', 'stage\\common.js', 'stage\\crt.js', 'stage\\connections.js', 'stage\\crtcontrols.js', 'stage\\filetransfer.js', 'stage\\graph.js', 'stage\\ftelnetclient.js'], 'release\\ftelnet.rip.xfer.js')
-	]).then(function() {
+		combineFiles(['stage/common.js', 'stage/crt.js', 'stage/connections.js', 'stage/ftelnetclient.js'], 'release/ftelnet.norip.noxfer.js'),
+		combineFiles(['include/blob.js', 'include/filesaver.js', 'stage/common.js', 'stage/crt.js', 'stage/connections.js', 'stage/crtcontrols.js', 'stage/filetransfer.js', 'stage/ftelnetclient.js'], 'release/ftelnet.norip.xfer.js'),
+		combineFiles(['stage/common.js', 'stage/crt.js', 'stage/connections.js', 'stage/graph.js', 'stage/ftelnetclient.js'], 'release/ftelnet.rip.noxfer.js'),
+		combineFiles(['include/blob.js', 'include/filesaver.js', 'stage/common.js', 'stage/crt.js', 'stage/connections.js', 'stage/crtcontrols.js', 'stage/filetransfer.js', 'stage/graph.js', 'stage/ftelnetclient.js'], 'release/ftelnet.rip.xfer.js')
+	]).then(function () {
 		Promise.all([
-			minifyFile('release\\ftelnet.norip.noxfer.js'),
-			minifyFile('release\\ftelnet.norip.xfer.js'),
-			minifyFile('release\\ftelnet.rip.noxfer.js'),
-			minifyFile('release\\ftelnet.rip.xfer.js')
+			minifyFile('release/ftelnet.norip.noxfer.js'),
+			minifyFile('release/ftelnet.norip.xfer.js'),
+			minifyFile('release/ftelnet.rip.noxfer.js'),
+			minifyFile('release/ftelnet.rip.xfer.js')
 		]);
 	});
 });
-
