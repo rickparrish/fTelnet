e11cc5ff0822cd6adb362bda0aa94acd1db3b648
rickparrish <github@rick.parrish>
2023-03-17 10:28:47 -0400

    Copy release files to embed wizard

--- build.cmd
+++ build.cmd
@@ -1,3 +1,10 @@
+@echo off
 cls
 node node_modules\typescript\bin\tsc --build source %1
 node postbuild.js
+
+rem Copy updated files to the embed wizard repo
+if exist C:\www\gh-pages\embed-v2.ftelnet.ca\ftelnet\ (
+  echo Copying updated files to the embed wizard repo
+  copy release\ftelnet*.js C:\www\gh-pages\embed-v2.ftelnet.ca\ftelnet
+)
