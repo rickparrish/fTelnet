263646bbe6c2044badee84e36f8bcf6336594c54
rickparrish <github@rick.parrish>
2025-05-05 17:43:53 -0400

    Update path

--- build.cmd
+++ build.cmd
@@ -4,7 +4,7 @@ node node_modules\typescript\bin\tsc --build source %1
 node postbuild.js
 
 rem Copy updated files to the embed wizard repo
-if exist C:\www\gh-pages\embed-v2.ftelnet.ca\ftelnet\ (
+if exist D:\www\gh-pages\embed-v2.ftelnet.ca\ftelnet\ (
   echo Copying updated files to the embed wizard repo
-  copy release\ftelnet*.js C:\www\gh-pages\embed-v2.ftelnet.ca\ftelnet
+  copy release\ftelnet*.js D:\www\gh-pages\embed-v2.ftelnet.ca\ftelnet
 )
